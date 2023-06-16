2b702ed57b819c9ccc6615849ad07d25dc56976c
Your Name <you@example.com>
2023-06-16 16:24:21 +0200

    minitalk

--- client.c
+++ client.c
@@ -6,26 +6,53 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 11:22:26 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/06/15 12:32:42 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:19:30 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft/include/libft.h"
 
-static void	signal_handler(int sig)
+void	send_sig(int pid, int sig)
 {
-	if (sig == SIGUSR1)
-		kill(SIGUSR1, sig);
-	if (sig == SIGUSR2)
-		kill(SIGUSR2, sig);
-	else
-		error_msg("received unexpected signal");
+	if (kill(pid, sig) == -1)
+	{
+		ft_printf("No connection with server");
+		exit(EXIT_FAILURE);
+	}
+	usleep(100);
+}
+
+void	send_bits(const int pid, char c, int bits)
+{
+	while (bits >= 0)
+	{
+		if (((c >> bits) & 1) == 1)
+			send_sig(pid, SIGUSR1);
+		else
+			send_sig(pid, SIGUSR2);
+		bits--;
+		usleep(1100);
+	}
+	ft_printf("\n");
 }
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int	pid;
+	int	i;
+
+	if (argc <= 2 || argc >= 4)
+	{
+		if (argc <= 2)
+			ft_printf("Too few arguments");
+		if (argc >= 4)
+			ft_printf("Too many arguments");
+		return (-1);
+	}
+	pid = ft_atoi(argv[1]);
+	i = -1;
+	while (argv[2][++i])
+		send_bits(pid, argv[2][i], 7);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,42 +6,50 @@
 /*   By: nkeyani- < nkeyani-@student.42barcelona    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 11:22:31 by nkeyani-          #+#    #+#             */
-/*   Updated: 2023/06/15 12:34:57 by nkeyani-         ###   ########.fr       */
+/*   Updated: 2023/06/16 16:20:51 by nkeyani-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include "libft/include/libft.h"
 
-static char	*error_msg(char *s)
+void	handler(int sig)
 {
-	ft_printf("%s", s);
-	exit(-1);
-}
+	static int				bits = 0;
+	static unsigned char	c = '\0';
 
-static unsigned char	*print_bits(int *bits)
-{
-	int	i;
-
-	i = 8
-	while (i--)
+	c <<= 1;
+	if (sig == SIGUSR1)
+		c |= 1;
+	bits++;
+	if (bits == 8)
 	{
-		if (SIGUSR1 == 1)
+		if ((int)c <= 126)
+			ft_printf("%c", c);
+		else if ((int)c >= 127)
+			ft_printf("%c", c);
+		bits = 0;
+		c = '\0';
 	}
 }
 
 int	main(void)
 {
-	if (signal(SIGUSR1, signal_handler) == -1)
-		error_msg("can't catch SIGUSR1");
-	if (signal(SIGUSR2, signal_handler) == -1)
-		error_msg("can't catch SIGUSR2");
-	ft_printf("PID of server: %d\n", getpid());
+	int	pid;
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	sa.sa_flags = SA_RESTART;
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		exit(EXIT_FAILURE);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(EXIT_FAILURE);
+	pid = getpid();
+	if (!pid)
+		ft_printf("PID NOT AVAILABLE RESTART THE SERVER");
+	else
+		ft_printf("Server PID: %i\n", getpid());
 	while (1)
-	{
-		signal(SIGUSR1, signal_handler);
-		signal(SIGUSR2, signal_handler);
-		pause();
-	}
+		sleep(1);
 	return (0);
 }
